f6ea8e40f6c7ef0c6c08665cd84d64d01c8635f9
LaiGiaBao <baolaigia110501@gmailc.com>
2021-06-23 17:49:44 +0700

    Use Layout in index and about sites

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,14 @@
 // Step 1: Import your component
 import * as React from 'react'
 import { Link} from 'gatsby'
+import Layout from '../pages/components/layout'
 
 // Step 2: Define your component
 const AboutPage = () => {
   return (
-    <main>
-      <title>About Me</title>
-      <h1>About Me</h1>
-      <Link to="/">Back to Home</Link>
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-    </main>
+    <Layout pageTitle="About">
+        <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout>
   )
 }
 
