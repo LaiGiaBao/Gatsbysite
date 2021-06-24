4c89963e94e1c4ab0a5f155d24c1d9729011ab2e
LaiGiaBao <baolaigia110501@gmailc.com>
2021-06-24 21:05:11 +0700

    First Task: Create a Blog

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import {Layout} from '../pages/components/layout'
+
+const BlogPage = () =>{
+    return (
+        <Layout pageTitle="My Blog Post">
+            <p>My cool posts will go in here</p>
+        </Layout>
+    )
+}
+export default BlogPage
