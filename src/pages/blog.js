caf537fb9af6c3e52445faeabc454818f6f65c98
LaiGiaBao <baolaigia110501@gmailc.com>
2021-06-25 00:38:13 +0700

    Learn how to use query

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,29 @@
+import { graphql } from 'gatsby'
 import * as React from 'react'
 import Layout from '../pages/components/layout'
 
-const BlogPage = () =>{
+const BlogPage = ({data}) =>{
     return (
         <Layout pageTitle="My Blog Page">
+            <p>My cool post wil go in here</p>
+            <ul>
+                {
+                    data.allFile.nodes.map((node) => (
+                        <li key={node.name}>{node.name}</li>
+                    ))
+                }
+            </ul>
         </Layout>
     )
 }
+export const query = graphql`
+query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+  
+`
 export default BlogPage
